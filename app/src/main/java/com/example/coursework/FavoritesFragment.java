8196b27d65cc3a415da9206f57336b6ce6b151d2
Emir <sabirov.e.g@edu.mirea.ru>
2025-03-29 13:14:17 +0300

    Добавлены новые фрагменты, иконки меню теперь меняют цвет при нажатии

--- app/src/main/java/com/example/coursework/FavoritesFragment.java
+++ app/src/main/java/com/example/coursework/FavoritesFragment.java
@@ -8,8 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class GalleryFragment extends Fragment {
-    public GalleryFragment() {}
+public class FavoritesFragment extends Fragment {
+
+    public FavoritesFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,6 +19,6 @@ public class GalleryFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+        return inflater.inflate(R.layout.fragment_favorites, container, false);
     }
 }
--- app/src/main/java/com/example/coursework/MainActivity.java
+++ app/src/main/java/com/example/coursework/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.coursework;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_bar);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.favorites){
+
+                replaceFragment(new FavoritesFragment());
+            }
+            else if (item.getItemId() == R.id.search){
+                replaceFragment(new SearchFragment());
+            }
+            else if (item.getItemId() == R.id.profile){
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
     }
 }
